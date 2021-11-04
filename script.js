fe530859b992f9232528cbb45c4d33c3a4797de4
glooneltharion <gloo.neltharion@gmail.com>
2021-11-04 19:46:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var foxOne = $('#fox-1'),
+    foxTwo = $('#fox-2'),
+    foxes = $('p'),
+    style = {
+        borderColor: "purple",
+        background: "yellow" 
+    };
+
+foxOne.text('Macrotis');
+
+foxTwo.css(style);
+
+foxes.css(style);
+
